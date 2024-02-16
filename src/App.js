fd4d95d63191fcbcbc7aedb228a7a299ebb13ac0
Aniket Biswas <aniketbiswas2392001@gmail.com>
2024-02-16 14:46:06 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -3,14 +3,22 @@ import Header from "./components/Header";
 import Navbar from "./components/Navbar/navbar";
 import LandingPage from "./components/landing/landing";
 import Footer from "./components/Footer/footer";
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LoginPage from './components/login/login';
 function App() {
     return (
-        <div className="App">
-            <Header/>
-            <Navbar />
-            <LandingPage />
-            <Footer />
-        </div>
+    <BrowserRouter>
+    <Header/>
+    <Navbar/>
+        <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="login" element={<LoginPage />} /> // Define the login route
+        </Routes>
+        <Footer />
+    </BrowserRouter>
+            
+            
     );
 }
 
--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './navbar.css';
 
 function Navbar(){
@@ -12,7 +13,7 @@ function Navbar(){
         <li className='navbar-item'><a href='#'>Kids</a></li>
         <li className='navbar-item'><a href='#'>Accessories</a></li>
         <li className='navbar-item'><div class="search-container"> <span class="material-symbols-outlined">search</span> <input type="text" placeholder="Search for products,brands and more..." class="search-input"/></div></li>
-        <li className='navbar-item'><a href='#'><span class="material-symbols-outlined">person</span>Login</a></li>
+        <li className='navbar-item'><Link to='/login'><span class="material-symbols-outlined">person</span>Login</Link></li>
         <li className='navbar-item'><a href='#'><span class="material-symbols-outlined">favorite</span>Wishlist</a></li>
         <li className='navbar-item'><a href='#'><span class="material-symbols-outlined">shopping_cart</span>Cart</a></li>
       </ul>
